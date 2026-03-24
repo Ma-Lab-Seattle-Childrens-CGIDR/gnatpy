f715e46aba9378670ef98adc63fa303073b3c420
Braden Griebel <bgriebel@uw.edu>
2026-03-24 10:23:52 -0700

    chore(initial commit)

---
+++ src/gnatpy/__init__.py
@@ -0,0 +1,26 @@
+from importlib.metadata import version
+
+__author__ = "Braden Griebel"
+__version__ = version("metworkpy")
+__all__ = [
+    "crane_gene_set_entropy",
+    "crane_gene_set_classification",
+    "dirac_gene_set_entropy",
+    "dirac_gene_set_classification",
+    "race_gene_set_entropy",
+    "infer_gene_set_entropy",
+    "CraneClassifier",
+    "DiracClassifier",
+]
+from .crane_functions import (
+    crane_gene_set_entropy,
+    crane_gene_set_classification,
+    CraneClassifier,
+)
+from .dirac_functions import (
+    dirac_gene_set_entropy,
+    dirac_gene_set_classification,
+    DiracClassifier,
+)
+from .race_functions import race_gene_set_entropy
+from .infer_functions import infer_gene_set_entropy
