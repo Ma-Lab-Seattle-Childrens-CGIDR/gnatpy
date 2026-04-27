f0263ddad24cdcf57df634d7bc2fde28824fd5c4
Braden Griebel <bgriebel@uw.edu>
2026-04-27 12:03:18 -0700

    feat(testing): Added method for generating AnnData instance Created a method for generating an AnnData with known ordering characteristics.

--- src/gnatpy/_datagen.py
+++ src/gnatpy/_datagen.py
@@ -7,9 +7,11 @@ from __future__ import annotations
 import functools
 from typing import Union, Optional, Tuple
 
-import numpy as np
 
 # External Imports
+import anndata as ad
+import numpy as np
+import pandas as pd
 from scipy.stats import rv_continuous, rv_discrete
 
 # Local Imports
@@ -25,8 +27,8 @@ Distribution = Union[rv_continuous, rv_discrete]
 def _generate_rank_entropy_data(
     n_ordered_samples: int,
     n_unordered_samples: int,
-    n_genes_ordered: int,
-    n_genes_unordered: int,
+    n_ordered_genes: int,
+    n_unordered_genes: int,
     dist: Distribution,
     noise_dist: Optional[Distribution] = None,
     noise_swaps: Optional[Union[float, int]] = None,
@@ -42,9 +44,9 @@ def _generate_rank_entropy_data(
         Number of ordered samples
     n_unordered_samples : int
         Number of unordered samples
-    n_genes_ordered : int
+    n_ordered_genes : int
         Number of ordered genes
-    n_genes_unordered : int
+    n_unordered_genes : int
         Number of unordered genes
     dist : Distribution
         Distribution to use for sampling, should be a scipy
@@ -86,14 +88,14 @@ def _generate_rank_entropy_data(
     rng = np.random.default_rng(seed=seed)
     ordered_array = _ordered_array(
         nrow=n_ordered_samples,
-        ncol=n_genes_ordered,
+        ncol=n_ordered_genes,
         dist=dist,
         col_shuffle=shuffle_genes,
         rng=rng,
     )
     unordered_array = _unordered_array(
         nrow=n_unordered_samples,
-        ncol=n_genes_ordered,
+        ncol=n_ordered_genes,
         dist=dist,
         rng=rng,
     )
@@ -113,21 +115,21 @@ def _generate_rank_entropy_data(
         )
     unordered_genes_array = _unordered_array(
         nrow=n_ordered_samples + n_unordered_samples,
-        ncol=n_genes_unordered,
+        ncol=n_unordered_genes,
         dist=dist,
         rng=rng,
     )
     res_array = np.hstack((ordered_genes_array, unordered_genes_array))
     if shuffle_genes:
-        genes_shuffled = rng.permuted(list(range(n_genes_ordered + n_genes_unordered)))
-        ordered_genes = genes_shuffled[:n_genes_ordered]
-        unordered_genes = genes_shuffled[n_genes_ordered:]
+        genes_shuffled = rng.permuted(list(range(n_ordered_genes + n_unordered_genes)))
+        ordered_genes = genes_shuffled[:n_ordered_genes]
+        unordered_genes = genes_shuffled[n_ordered_genes:]
         res_array[:, ordered_genes] = ordered_genes_array
         res_array[:, unordered_genes] = unordered_genes_array
     else:
-        ordered_genes = np.array(range(n_genes_ordered))
+        ordered_genes = np.array(range(n_ordered_genes))
         unordered_genes = np.array(
-            range(n_genes_ordered, n_genes_unordered + n_genes_ordered)
+            range(n_ordered_genes, n_unordered_genes + n_ordered_genes)
         )
     if noise_dist is not None:
         res_array = _noise_addition(res_array, dist=noise_dist, rng=rng)
@@ -140,6 +142,98 @@ def _generate_rank_entropy_data(
         ordered_genes,
         unordered_genes,
     )
+
+
+def _generate_rank_entropy_anndata(
+    n_ordered_samples: int,
+    n_unordered_samples: int,
+    n_ordered_genes: int,
+    n_unordered_genes: int,
+    dist: Distribution,
+    noise_dist: Optional[Distribution] = None,
+    noise_swaps: Optional[Union[float, int]] = None,
+    shuffle_genes: bool = True,
+    shuffle_samples: bool = True,
+    seed: Optional[int] = None,
+) -> ad.AnnData:
+    """Generate data with ordered and disordered genes/samples
+
+    Parameters
+    ----------
+    n_ordered_samples : int
+        Number of ordered samples
+    n_unordered_samples : int
+        Number of unordered samples
+    n_ordered_genes : int
+        Number of ordered genes
+    n_unordered_genes : int
+        Number of unordered genes
+    dist : Distribution
+        Distribution to use for sampling, should be a scipy
+        rv_continuous, or rv_discrete (or at least have a rvs method
+        which matches the SciPy API). The seed to this function
+        will be used to create an RNG which is passed as the
+        random state to all calls to the rvs function.
+    noise_dist : Distribution, optional
+        Distribution to use for adding noise to the samples,
+        used to generate random values for each entry in the expression
+        array, and these values are added to the array
+    noise_swaps : float or int, optional
+        Number of random swaps to perform. Randomly selects a row, and then
+        swaps two elements in that row. Can be an integer
+        in which case that is the number of swaps, or a float
+        between 0 and 1 in which case it is the proportion of the
+        number of values in the resulting array to swap
+        (note that it randomly swaps, so it could repeat some swaps
+        so this proportion won't be the number of elements that are swapped,
+        only the number of swaps that are performed). The swapping using the
+        seed argument to seed its rng.
+    shuffle_genes : bool
+        Whether the order of the genes should be shuffled
+    shuffle_samples : bool
+        Whether the order of the samples should be shuffled
+    seed : Optional[int]
+        Seed to use for the random number generator used whe shuffling
+        (doesn't change the sampling from the provided dist)
+
+    Returns
+    -------
+    ad.AnnData
+        AnnData with the generated expression data. The observations have metadata ('sample_type')
+        which is 'ordered'/'unordered' to indicate the samples which are ordered. Simmilarly, the
+        vars has metadata ('gene_type') which is 'ordered'/'unordered' to indicate which genes
+        are ordered.
+    """
+    expr_data, ordered_samples, unordered_samples, ordered_genes, unordered_genes = (
+        _generate_rank_entropy_data(
+            n_ordered_samples=n_ordered_samples,
+            n_unordered_samples=n_unordered_samples,
+            n_ordered_genes=n_ordered_genes,
+            n_unordered_genes=n_unordered_genes,
+            dist=dist,
+            noise_dist=noise_dist,
+            noise_swaps=noise_swaps,
+            shuffle_genes=shuffle_genes,
+            shuffle_samples=shuffle_samples,
+            seed=seed,
+        )
+    )
+    adata = ad.AnnData(expr_data)
+    # Add in cell and gene names (just by index)
+    adata.obs_names = [f"Cell_{i:d}" for i in range(adata.n_obs)]
+    adata.var_names = [f"Gene_{i:d}" for i in range(adata.n_vars)]
+    # Add in annotations for which samples are ordered vs unordered
+    sample_types = np.empty((adata.n_obs), dtype=np.dtypes.StringDType)
+    sample_types[ordered_samples] = "ordered"
+    sample_types[unordered_samples] = "unordered"
+    adata.obs["sample_type"] = pd.Categorical(sample_types)
+    # Add in annotations for which genes are ordered vs unordered
+    gene_types = np.empty((adata.n_vars), dtype=np.dtypes.StringDType)
+    gene_types[ordered_genes] = "ordered"
+    gene_types[unordered_genes] = "unordered"
+    adata.var["gene_type"] = pd.Categorical(gene_types)
+    # Return the final anndata
+    return adata
 
 
 # endregion Main Function
